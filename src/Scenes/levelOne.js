193d4084729da82466b2e45b3fb0e22bef5b0b51
Thomas Wessel <turpleturtle12@gmail.com>
2024-05-05 22:27:50 -0700

    lots of stuff

--- src/Scenes/levelOne.js
+++ src/Scenes/levelOne.js
@@ -11,8 +11,9 @@ class levelOne extends BasicShootingScene {
         //Enemies
         this.load.image('duck_clean', "ducks/PNG/Objects/duck_yellow.png");
         this.load.image('duck_dirty', "custom/duck_dirty.png");
-        this.load.image('duck_health', "jumper/PNG/Enemies/spikeMan_stand.png");
         this.load.image('dirt_projectile', 'jumper/PNG/Particles/particle_brown.png');
+        this.load.image('duck_health', "custom/duck_health.png");
+        this.load.image('health_pack', "shmup/Tiles/tile_0024.png");
 
         this.load.json('enemyBehavior', 'enemyBehavior.json');
 
@@ -22,50 +23,6 @@ class levelOne extends BasicShootingScene {
 
     create() {
         super.create();
-
-        this.points = [
-            50, 21,
-            336, 24,
-            702, 24,
-            925, 24,
-            977, 64,
-            972, 111,
-            782, 125,
-            601, 110,
-            420, 105,
-            264, 111,
-            75, 115,
-            21, 170,
-            71, 232,
-            247, 229,
-            438, 224,
-            622, 220,
-            797, 219,
-            972, 210,
-            994, 264,
-            976, 310,
-            795, 315,
-            595, 314,
-            439, 324,
-            239, 325,
-            59, 325,
-            9, 387,
-            73, 441,
-            228, 422,
-            454, 435,
-            615, 412,
-            810, 402,
-            987, 397,
-            995, 455,
-            938, 503,
-            767, 518,
-            605, 529,
-            445, 532,
-            278, 530,
-            97, 535,
-            38, 537
-        ];
-        this.curve = new Phaser.Curves.Spline(this.points);
 
         this.totalDucks = 100;
         this.enemyBehavior = this.cache.json.get('enemyBehavior');
@@ -88,7 +45,7 @@ class levelOne extends BasicShootingScene {
         super.update();
         this.doCollisionCheck();
 
-        //check to see if this.enemyShip is at the end of the path
+
         for (let i = 0; i < this.enemyArray.length; i++) {
 
             //randomly decide to spawn projectile
@@ -97,13 +54,13 @@ class levelOne extends BasicShootingScene {
             }
 
             this.spawnLowDuck(this.enemyArray[i]);
+
         }
 
         //spawn more ducks randomly
         if (this.totalDucks > 0 && Math.random() < this.spawnRate) {
             this.spawnRandomDuck();
             this.spawnRate += 0.00005;
-            this.totalDucks--;
         }
 
 
@@ -141,6 +98,12 @@ class levelOne extends BasicShootingScene {
         for (let i = 0; i < this.playerProjectileArray.length; i++) {
             for (let j = 0; j < this.enemyArray.length; j++) {
                 if (this.doesOverlap(this.playerProjectileArray[i], this.enemyArray[j])) {
+                    //if it is a health duck, drop a health pack
+                    if(this.enemyArray[j].getData("isHealth")){
+                        this.spawnPlayerTargeted(this.enemyArray[j], "health_pack", this.enemyProjectileArray);
+                    }
+
+
                     this.playerProjectileArray[i].destroy();
                     this.enemyArray[j].destroy();
                     this.playerProjectileArray.splice(i, 1);
@@ -155,9 +118,13 @@ class levelOne extends BasicShootingScene {
         //check for collision between enemy projectiles and player ship
         for (let i = 0; i < this.enemyProjectileArray.length; i++) {
             if (this.doesOverlap(this.enemyProjectileArray[i], this.playerSprite)) {
+                if(this.enemyProjectileArray[i].getData("isHealth")){
+                    this.lives++;
+                }else{
+                   this.lives--;
+                }
                 this.enemyProjectileArray[i].destroy();
                 this.enemyProjectileArray.splice(i, 1);
-                this.lives--;
                 break;
             }
         }
@@ -176,11 +143,11 @@ class levelOne extends BasicShootingScene {
     spawnLowDuck(sprite) {
         if (sprite.y >= 400) {
             let tempSpline = this.createExtendedSpline(sprite, this.playerSprite);
-            this.enemyShip2 = this.add.follower(tempSpline, sprite.x, sprite.y, "duck_dirty");
-            this.enemyShip2.setScale(0.5);
-            this.enemyShip2.startFollow(
+            let newProjectile = this.add.follower(tempSpline, sprite.x, sprite.y, "duck_dirty");
+            newProjectile.setScale(0.5);
+            newProjectile.startFollow(
                 {
-                    duration: 10000,
+                    duration: 1000,
                     yoyo: false,
                     repeat: 0
                 }
@@ -188,6 +155,8 @@ class levelOne extends BasicShootingScene {
             sprite.destroy();
             //remove that sprite from the array
             this.enemyArray.splice(this.enemyArray.indexOf(sprite), 1);
+            //add the new sprite to the array
+            this.enemyProjectileArray.push(newProjectile);
             sprite.y = -99;
 
         }
@@ -195,20 +164,35 @@ class levelOne extends BasicShootingScene {
 
     spawnPlayerTargeted(sourceSprite, texture, array) {
         let tempSpline = this.createExtendedSpline(sourceSprite, this.playerSprite);
-        this.enemyShip2 = this.add.follower(tempSpline, sourceSprite.x, sourceSprite.y, texture);
-        this.enemyShip2.setScale(0.5);
-        this.enemyShip2.startFollow(
+        let newProjectile = this.add.follower(tempSpline, sourceSprite.x, sourceSprite.y, texture);
+        if(texture === "health_pack"){
+            newProjectile.setScale(2);
+            newProjectile.setData("isHealth", true);
+        }else{
+            newProjectile.setScale(0.5);
+        }
+
+        newProjectile.startFollow(
             {
                 duration: 10000,
                 yoyo: false,
                 repeat: 0
             }
         )
-        array.push(this.enemyShip2);
+        array.push(newProjectile);
     }
 
     spawnRandomDuck() {
         let behavior = Phaser.Utils.Array.GetRandom(this.enemyBehavior);
+        if(behavior.name === "duck_health"){
+            //make this happen only a tiny amt of the time
+            if(Math.random() < 0.07){
+                return;
+            }else{
+                console.log("duck_health")
+            }
+        }
+        this.totalDucks--;
         let spline = new Phaser.Curves.Spline(behavior.path);
 
         // Get the first coordinate of the path
@@ -216,15 +200,23 @@ class levelOne extends BasicShootingScene {
         let startY = behavior.path[1];
 
         // Spawn a new duck at the first coordinate of the path
-        let enemyShip = this.add.follower(spline, startX, startY, behavior.name);
-        enemyShip.setScale(0.5);
-        enemyShip.startFollow({
+        let newDuck = this.add.follower(spline, startX, startY, behavior.name);
+        newDuck.setScale(0.5);
+        newDuck.startFollow({
             duration: 10000,
             yoyo: false,
             repeat: 0
         });
 
-        this.enemyArray.push(enemyShip);
+        //and if it is a duck_health, give it some data
+        if(behavior.name === "duck_health"){
+            newDuck.setData("isHealth", true);
+        }else{
+            newDuck.setData("isHealth", false);
+        }
+
+        this.enemyArray.push(newDuck);
+        console.log(this.totalDucks);
     }
 
 }
